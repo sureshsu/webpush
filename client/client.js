b7b0e60ad63b5df874c3a53928ea3898fdc98dbb
sureshsu <suresh.pgsem2008@gmail.com>
2023-02-17 15:13:07 +0530

    updated

--- client/client.js
+++ client/client.js
@@ -12,9 +12,31 @@ async function registerServiceWorker() {
 
     console.log("service worker = "+ register);
 
-    const subscription = await register.pushManager.subscribe({
-        userVisibleOnly: true,
-        applicationServerKey: publicVapidKey,
+//     const subscription = await register.pushManager.subscribe({
+//         userVisibleOnly: true,
+//         applicationServerKey: publicVapidKey,
+//     });
+    
+    navigator.serviceWorker.ready.then(
+      (serviceWorkerRegistration) => {
+            const options = {
+              userVisibleOnly: true,
+              applicationServerKey: publicVapidKey,
+            };
+            serviceWorkerRegistration.pushManager.subscribe(options).then(
+              (pushSubscription) => {
+                console.log(pushSubscription.endpoint);
+                // The push subscription details needed by the application
+                // server are now available, and can be sent to it using,
+                // for example, an XMLHttpRequest.
+              }, (error) => {
+                // During development it often helps to log errors to the
+                // console. In a production environment it might make sense to
+                // also report information about errors back to the
+                // application server.
+                console.error(error);
+              }
+            );
     });
 
     console.log("pushManager subscription = "+ subscription);
